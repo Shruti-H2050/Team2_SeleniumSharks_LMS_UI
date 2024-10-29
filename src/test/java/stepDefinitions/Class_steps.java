794b59515a6f1c0e700647f1b1bcf2c05beb6d26
Shruti <shruti@MacBook-Pro.local>
2024-10-29 11:47:57 -0500

    getclassurl

--- src/test/java/stepDefinitions/Class_steps.java
+++ src/test/java/stepDefinitions/Class_steps.java
@@ -27,7 +27,7 @@ public class Class_steps {
 	@Given("Admin is on the dashboard page after login")
 	public void admin_is_on_the_dashboard_page_after_login() throws InterruptedException {
 		 context.launchBrowser();
-	        context.launchLoginUrl();
+	        context.launchUrl();
 	        loginPage.Login(context.getPropUsername(),context.getPropPassword());
 	       // loginPage.Login("Sdet@gmail.com", "LmsHackathon@2024");
 	       // Thread.sleep(2000);
@@ -131,8 +131,77 @@ public class Class_steps {
 		 context.getClassURL();
 	}
 
-
-
+	@Given("Admin is on the Edit Class Popup window")
+	public void admin_is_on_the_edit_class_popup_window() {
+		classPage.justClick();
+		classPage.editDialog();
+	}
+	@When("Admin clicks Cancel button on edit popup")
+	public void admin_clicks_cancel_button_on_edit_popup() {
+	   
+	}
+	@Then("Admin can see the class details popup disappears and can see nothing changed for particular Class")
+	public void admin_can_see_the_class_details_popup_disappears_and_can_see_nothing_changed_for_particular_class() {
+	   
+	}
+	@When("Admin clicks on the edit icon")
+	public void admin_clicks_on_the_edit_icon() {
+		classPage.justClick();
+		classPage.clickeditIcon();
+	}
+	@Then("A new pop up with class details appears")
+	public void a_new_pop_up_with_class_details_appears() {
+	   classPage.editDialog();
+	}
+	
+	@Then("Admin should see the batch name field is disabled")
+	public void admin_should_see_the_batch_name_field_is_disabled() {
+		classPage.pgmFieldDisabledforediting();
+	}
+	
+	@When("Update the fields with invalid values and click save")
+	public void update_the_fields_with_invalid_values_and_click_save() {
+	  
+	}
+	@Then("Admin should get Error message")
+	public void admin_should_get_error_message() {
+	    
+	}
+	
+	@When("Update the mandatory fields with valid values and click save")
+	public void update_the_mandatory_fields_with_valid_values_and_click_save() {
+	    
+	}
+	
+	
+	@Then("Admin should see the class topic field is disabled")
+	public void admin_should_see_the_class_topic_field_is_disabled() {
+		classPage.classTopicFieldDisabledforediting();
+	}
+	
+	@When("Update the optional fields with valid values and click save")
+	public void update_the_optional_fields_with_valid_values_and_click_save() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@When("Update the fields with valid data and click save")
+	public void update_the_fields_with_valid_data_and_click_save() {
+		classPage.classDescField();
+		classPage.classCmntsField();
+		classPage.clickSave();
+	}
+	@Then("Admin gets message {string} and see the updated values in data table")
+	public void admin_gets_message_and_see_the_updated_values_in_data_table(String string) {
+	    
+	}
+	
+	@When("Admin enters only numbers or special char in the text fields")
+	public void admin_enters_only_numbers_or_special_char_in_the_text_fields() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
 }
 
 
